dbeb2f47b75524cf4155d83decf0adf589373572
jvbrates <jv.belmonterates@gmail.com>
2023-09-22 14:18:01 -0300

    Fazendo tudo de novo porque deletei a pasta errada

--- Trabalhos/t1/util/linked_list.c
+++ Trabalhos/t1/util/linked_list.c
@@ -1,28 +1,97 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct node node_t;
+typedef struct node_t node_t;
 
-struct node {
+struct node_t {
   unsigned int key;
   void *packet;
   node_t *next;
   node_t *previous;
 };
 
+node_t *llist_create_node(void *packet, unsigned int key){
+    node_t *node = malloc(sizeof(node_t));
+    if(node) {
+      node->packet = packet;
+      node->key = key;
+      node->next = NULL;
+      node->previous = NULL;
+    }
+    return node;
+}
+
+node_t *llist_create_node_round(void *packet, unsigned int key){
+    node_t *node = malloc(sizeof(node_t));
+    if(node) {
+      node->packet = packet;
+      node->key = key;
+      node->next = node;
+      node->previous = node;
+    }
+    return node;
+}
+
+node_t *llist_get_next(node_t *node){
+    if(node)
+      return node->next;
+    return NULL;
+}
+
+node_t *llist_get_previous(node_t*){
+    if(node)
+      return node->previous;
+    return NULL;
+}
+
+void *llist_get_packet(node_t *node){
+    if (node)
+      return node->packet;
+}
+
+void *llist_node_search(node_t *first, unsigned int PID);
+
+int llist_add_node_next(node_t *self, node_t *next){
+
+    node_t  *self_old_next = self->next;
+    self->next = next;
+    next->previous = self;
+    next->next = self_old_next;
+    if(self_old_next)
+      self_old_next->previous = next;
+
+    return 0;
+}
+
+int llist_add_node_previous(node_t* self, node_t *previous){
+
+    node_t  *self_old_previous = self->previous;
+    self->previous = previous;
+    previous->next = self;
+    previous->previous = self_old_previous;
+    if(self_old_previous)
+      self_old_previous->next =  previous;
+
+    return 0;
+}
+
+int llist_node_unlink(node_t *node){
+    if(node->next)
+      node->next->previous = node->previous;
+    if(node->previous)
+      node->previous->next = node->next;
+
+    return 0;
+}
+
+node_t *llist_add_node(node_t **node_holder, node_t *node);
+
+node_t *llist_delete_node(node_t **node_holder, unsigned int key);
+
 
 void delete_node(node_t *node){
   if (node)
     free(node);
-}
-
-node_t * newNode(void * packet, unsigned int key){
-  node_t *ret = malloc(sizeof (node_t));
-  ret->packet = packet;
-  ret->key = key;
-  ret->next = NULL;
-  ret->previous = NULL;
-  return ret;
 }
 
 
--- Trabalhos/t1/util/linked_list.c.h
+++ Trabalhos/t1/util/linked_list.c.h
@@ -5,4 +5,27 @@
 #ifndef T1_LINKED_LIST_C_H
 #define T1_LINKED_LIST_C_H
 
+typedef struct node_t node_t;
+
+node_t *llist_create_node(void *packet, unsigned int key);
+node_t *llist_create_node_round(void *packet, unsigned int key);
+
+node_t *llist_get_next(node_t*);
+
+node_t *llist_get_previous(node_t*);
+
+void *llist_get_packet(node_t *);
+
+void *llist_node_search(node_t *first, unsigned int PID);
+
+int llist_add_node_next(node_t *next);
+
+int llist_add_node_previous(node_t *next);
+
+int llist_node_unlink(node_t *node);
+
+node_t *llist_add_node(node_t **node_holder, node_t *node);
+
+node_t *llist_delete_node(node_t **node_holder, unsigned int key);
+
 #endif // T1_LINKED_LIST_C_H
